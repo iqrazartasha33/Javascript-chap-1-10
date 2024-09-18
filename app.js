58cfbf91acf2512331f092e38049e5803df0abd1
iqrazartasha33 <iqrazartasha33@gmail.com>
2024-09-18 20:17:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// Que No 01 //
+var myName="Iqra";
+alert(myName);
+
+// Que No 02 //
+var fullname = "Iqra Shamim";
+alert("Hello, My Name is" +" " +fullname);
+console.log(fullname);
+
+// Que No 03 //
+var a=4;
+var b=2;
+var result = (a++ + --b - --a + b++ + a + b);
+console.log(result);
+
+//Que No 04 //
+var age = prompt ("How old are you?")
+ if (age<= 13){
+    alert("You are a child");
+ } else if (age>=13 && age<= 18){
+    alert("You are a teenger");
+ } else {
+    alert("You are an adult");
+ }
